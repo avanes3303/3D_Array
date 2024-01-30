d4193671d6c8d0f29195b36deefa79c1c3bdf4a9
avanes3303 <144267381+avanes3303@users.noreply.github.com>
2024-01-30 03:55:26 +0300

    done

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/abcde/3D_Array/test
-# Build directory: C:/Users/abcde/3D_Array/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/abcde/3D_Array/cmake-build-debug/test/tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
